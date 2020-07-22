a63d97ae665f3ce358fbe63b66e6b5fb78c104e4
Mark Zelou Garces <mg7855@us.att.com>
2020-07-22 19:26:17 +0800

    optimize code

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 	hostnames := util.ReadLines(strings.TrimSpace(arg2))
 
 	fmt.Print("validating records...\n\n")
-	fmt.Print("[PING] - ping [HTTP GET] - port 80 [HTTPS GET] - port 443 [Dial Timeout] - port 80\n\n[logs]\n")
+	fmt.Print("[Test Methods]:\nPING: ping\nHTTP GET: port 80\nHTTPS GET: port 443\nDial Timeout: port 80\n\n[logs]\n")
 	// validate records
 	validate.Validate(ips, hostnames)
 
